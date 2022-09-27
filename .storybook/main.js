d4888cf31723c5d6767d81530d35638cdb935462
asiemi <asiemi@softserveinc.com>
2022-09-27 12:16:54 +0200

    design tab visibility fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook-addon-designs",
+    "storybook-addon-designs"
   ],
   framework: "@storybook/angular",
   core: {
